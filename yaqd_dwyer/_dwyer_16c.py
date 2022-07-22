72c95d91fc0676439b4afa14b5a681b8e222d18a
Blaise Thompson <blaise@untzag.com>
2022-07-22 11:34:50 -0500

    support dwyer 16c

--- yaqd_dwyer/_dwyer_16c.py
+++ yaqd_dwyer/_dwyer_16c.py
@@ -15,15 +15,20 @@ def int2temp(value):
     return value / 10
 
 
-def temp2value(value):
+def temp2int(value):
     value = int(value * 10)
     if value < 0:
-        value += 2**15
+        value += 2**16
     return value
 
 
+parity_options = {"even": "E", "odd": "O"}
+
+stop_bit_options = {"one": 1, "one_and_half": 1.5, "two": 2}
+
+
 class Dwyer16C(HasLimits, HasPosition, UsesUart, UsesSerial, IsDaemon):
-    _kind = "dwyer-16b"
+    _kind = "dwyer-16c"
 
     def __init__(self, name, config, config_filepath):
         super().__init__(name, config, config_filepath)
@@ -32,26 +37,23 @@ class Dwyer16C(HasLimits, HasPosition, UsesUart, UsesSerial, IsDaemon):
         )
         self._instrument.serial.baudrate = self._config["baud_rate"]
         self._instrument.serial.bytesize = self._config["byte_size"]
-        self._instrument.serial.stopbits = self._config["stop_bits"]
-        self._instrument.serial.parity = self._config["parity"]
+        self._instrument.serial.stopbits = stop_bit_options[self._config["stop_bits"]]
+        self._instrument.serial.parity = parity_options[self._config["parity"]]
         self._instrument.handle_local_echo = self._config["modbus_handle_echo"]
-        # ensure that control method is PID
-        self._instrument.write_register(0x1005, 0)
         # units
-        if self._instrument.read_bit(0x0811):
+        if self._instrument.read_register(0x4717):
             self._units = "deg_C"
         else:
             self._units = "deg_F"
         # limits
-        self._state["hw_limits"][0] = int2temp(self._instrument.read_register(0x1003))
-        self._state["hw_limits"][1] = int2temp(self._instrument.read_register(0x1002))
+        self._state["hw_limits"][0] = int2temp(self._instrument.read_register(0x4707))
+        self._state["hw_limits"][1] = int2temp(self._instrument.read_register(0x4706))
 
     def direct_serial_write(self, message: bytes):
         self._instrument.serial.write(message)
 
     def _set_position(self, position: float):
-        value = int(position * 10)
-        self._instrument.write_register(0x1001, value)
+        self._instrument.write_register(0x4701, temp2int(position), functioncode=6)
 
     async def update_state(self):
         """Continually monitor and update the current daemon state."""
@@ -59,9 +61,9 @@ class Dwyer16C(HasLimits, HasPosition, UsesUart, UsesSerial, IsDaemon):
         while True:
             try:
                 self._busy = False
-                registers = self._instrument.read_registers(0x1000, 5)
-                self._state["position"] = registers[0] / 10
-                self._state["destination"] = registers[1] / 10
+                registers = self._instrument.read_registers(0x4700, 5)
+                self._state["position"] = int2temp(registers[0])
+                self._state["destination"] = int2temp(registers[1])
                 await asyncio.sleep(0.25)
             except minimalmodbus.LocalEchoError:
                 continue
